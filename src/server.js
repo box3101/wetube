f056aec45ea7342ca1c77e98bf42577fe80f323c
box3101 <box4101@nate.com>
2025-01-30 01:15:32 +0900

    5강. Express로 첫 라우터 만들기! 🚀

--- src/server.js
+++ src/server.js
@@ -3,6 +3,19 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+
+const handleHome = (req, res) => {
+  return res.send("hello");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("login");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+
 app.listen(PORT, handleListening);
 
 function handleListening() {
