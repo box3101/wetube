11fe07e557e13ef3c5106c188de48706b0fc15ea
box3101 <box4101@nate.com>
2025-01-30 12:49:49 +0900

    6r강 Express 미들웨어 마스터하기! 🎯

--- src/server.js
+++ src/server.js
@@ -3,21 +3,20 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
 const handleHome = (req, res) => {
-  return res.send("hello");
+  return res.end();
 };
 
-const handleLogin = (req, res) => {
-  return res.send("login");
-};
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
+app.get("/", logger, handleHome);
 
 app.listen(PORT, handleListening);
 
 function handleListening() {
-  console.log("Server is running on port 4000");
+  console.log(`Server is running on port ${PORT}`);
 }
