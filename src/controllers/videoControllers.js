3e1627437a938fa2569b8a36a14dc65257bc90fd
box3101 <box4101@nate.com>
2025-02-01 05:15:35 +0900

    10강 Pug로 View 만들기! 🎨

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,11 +1,7 @@
-const handleTrending = (req, res) => res.send("Trending");
-
-const handleSeeVideo = (req, res) => {
-  console.log(req.params);
-  res.send("See Video");
-}
+const handleTrending = (req, res) => res.render("home");
+const handleSeeVideo = (req, res) => res.render("watch");
 const handleSearch = (req, res) => res.send("Search");
-const handleEditVideo = (req, res) => res.send("Edit Video");
+const handleEditVideo = (req, res) => res.render("edit");
 const handleDeleteVideo = (req, res) => res.send("Delete Video");
 const handleUploadVideo = (req, res) => res.send("Upload Video");
 
