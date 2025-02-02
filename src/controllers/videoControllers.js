df021152249ef3b369d02b8f3530c0a099eb7221
box3101 <box4101@nate.com>
2025-02-02 13:58:16 +0900

    12강.Express에서 가짜 데이터베이스 만들기! 🎯

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,14 +1,43 @@
-const fakeUser = {
-  username: "nico",
-  loggedIn: false,
-};
+const videos =[
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+]
 
+const handleTrending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos })
+}
 
-const handleTrending = (req, res) => res.render("home" , 
-  {pageTitle: "Home" , fakeUser} 
-);
-const handleSeeVideo = (req, res) => res.render("watch");
+const handleSeeVideo = (req, res) => {
+  const {id} = req.params;
+  const video = videos[id-1];
+  return res.render("watch", { pageTitle: `Watch ${video.title} Video`});
+}
 const handleSearch = (req, res) => res.send("Search");
+
+
+
 const handleEditVideo = (req, res) => res.render("edit");
 const handleDeleteVideo = (req, res) => res.send("Delete Video");
 const handleUploadVideo = (req, res) => res.send("Upload Video");
